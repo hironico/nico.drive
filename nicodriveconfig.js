78655322765c8ae749a83d9a0b09d22589f7db68
Nicolas RAMOUSSE <hironico.pub@outlook.com>
2019-04-01 21:43:07 +0200

    server and its config file

--- nicodriveconfig.js
+++ nicodriveconfig.js
@@ -1,6 +1,6 @@
 var nicodriveconfig = module.exports = {
     rootDir: "/data/synobackup/files",
-    lockdir: "/data/synobackup/files",
+    locksdir: "/data/synobackup/files",
     auth: {
         digestFile: "./htdigest",
         realm: "synobackup"
