38d9ea2a218eac2bbcbc6e9d3f026eb56abb669a
Nicolas RAMOUSSE <hironico.pub@outlook.com>
2020-11-06 22:01:20 +0100

    Cleanup master branch before new app

--- start-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOGFILE="/var/log/sites/synobackup/nodejs.log"
-
-echo "Log file is: $LOGFILE"
-
-nohup node nicodriveserver.js > $LOGFILE 2>&1 &
-echo $! > nico.drive.pid
-echo "Server started !"
