477dd327a043a108598af32e9542b719f0514d9c
hironico <hironico.pub@outlook.com>
2021-03-01 13:49:12 +0100

    whois does not send the password in the response !

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,13 @@
 import * as express from "express";
 import { IUser } from "webdav-server";
 
+class UserProfile {
+    uid: string;
+    username: string;
+    isAdministrator: boolean;
+    isDefaultUser: boolean;
+}
+
 export const register = (app: express.Application) : void => {
 
     const userManager = app.locals.userManager;
@@ -8,10 +15,18 @@ export const register = (app: express.Application) : void => {
     app.get("/auth/whois/:username", (req, res) => {
         const username = req.params['username'];
 
-        console.log(`Whois {username}`);
-
         userManager.getUserByName(username, (error: Error, user: IUser) => {
-            user ? res.status(200).send(JSON.stringify(user)) : res.status(404).send("User not found");
+            if (user) {
+                const userProfile: UserProfile = new UserProfile();
+                userProfile.uid = user.uid;
+                userProfile.username = user.username;
+                userProfile.isAdministrator = user.isAdministrator;
+                userProfile.isDefaultUser = user.isDefaultUser;
+
+                res.status(200).send(userProfile);
+            } else {
+                res.status(404).send("User not found");
+            }
         });
     });
 };
