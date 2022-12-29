d42d3a14e5ff2dc36531949e3e0fecb6ee0d6d12
hironico <hironico@github.com>
2022-12-29 15:50:23 +0100

    typo

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ mkdir -p $LOG_DIR
 # make sure the libraw.so library is accessible in the LD LIB PATH.
 if [ -d ./tools/ ]
 then
-    export LD_LIBRARY_PATH=./tools/.;$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=./tools/.:$LD_LIBRARY_PATH
 fi
 
 nohup node . > $LOG_FILE 2>&1 &
