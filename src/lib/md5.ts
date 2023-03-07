93d5cd11e2dc91aca3bd821429166b981bb88dda
hironico <hironico.pub@outlook.com>
2023-03-07 17:44:04 +0100

    use const variables in md5 lib code

--- src/lib/md5.ts
+++ src/lib/md5.ts
@@ -5,17 +5,17 @@ const md5 = (fileName: string): Promise<string> => {
     return new Promise<string>((resolve, reject) => {
         try {
             // créer un objet Hash avec l'algorithme md5
-            var hash = crypto.createHash('md5');
+            const hash = crypto.createHash('md5');
 
             // créer un stream de lecture à partir du fichier
-            var stream = fs.createReadStream(fileName);
+            const stream = fs.createReadStream(fileName);
 
             // écrire les données du stream dans le hash
             stream.pipe(hash);
 
             // quand le stream est terminé, lire le hash md5
             stream.on('end', function() {
-                var md5 = hash.digest('hex');
+                const md5 = hash.digest('hex');
                 resolve(md5);
             });
         } catch(error) {
