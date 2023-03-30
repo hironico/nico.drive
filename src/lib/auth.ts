c5685e5977a54ea92ad1e71e76140b76b6b85d53
hironico <hironico@github.com>
2023-03-30 20:52:56 +0200

    user role testing for a given root dir

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -20,3 +20,28 @@ export const basicAuthHandler = (usr: string, pwd: string) : boolean => {
     const user = matchingUsers[0];
     return user.password === pwd;
 }
+
+/**
+ * Test if a user has a certain role on a root directory name.
+ * @param usr tested user
+ * @param role the tested role against the user is tested
+ * @param rootDirName root dir name on which we want to know if this user has a role
+ * @returns true if the user exists and the root directory exists for that user and the user has that role on this root director. false otherwise.
+ */
+export const hasRole = (usr: string, role: string, rootDirName: string): boolean => {
+    const matchingUsers = usersConfig.users.filter(user => user.username === usr);
+    if (typeof matchingUsers === 'undefined' || matchingUsers.length !== 1) {
+        return false;
+    }
+
+    const user = matchingUsers[0];
+
+    const matchingRootDirs = user.rootDirectories.filter(rootDir => rootDir.name === rootDirName);
+    if (typeof matchingRootDirs === 'undefined' || matchingRootDirs.length !== 1) {
+        return false;
+    }
+
+    const rootDir = matchingRootDirs[0];
+
+    return rootDir.roles.indexOf(role) >= 0;
+}
