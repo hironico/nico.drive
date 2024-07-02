3d9e7aa4002d65d71aeb57d6fd1c9eaa1e179a36
hironico <hironico.pub@outlook.com>
2024-07-02 11:49:17 +0200

    one publish channel per thread

--- src/lib/rabbit_thumbgen.ts
+++ src/lib/rabbit_thumbgen.ts
@@ -13,34 +13,35 @@ const queue = process.env.THUMBS_REQUEST_QUEUE_NAME;
 const prefetchSize = parseInt(process.env.THUMBS_REQUEST_QUEUE_PREFETCH);
 
 let subscribe_channel: amqp.Channel;
-let publish_channel: amqp.Channel;
 
 const buildThumbFromMessageAsync = (msg: amqp.ConsumeMessage) => {
-    const strContent: string = msg.content.toString();    
+    console.log('Received a thumb request message.');
+    const strContent: string = msg.content.toString();
     console.log(" [x] Received %s", strContent);
 
-    const request: ThumbRequest = JSON.parse(strContent);    
+    const request: ThumbRequest = JSON.parse(strContent);
     const w = new Worker('./src/lib/workerlauncher_thumbgen.js', {
         workerData: {
-          request: request
+            request: request
         }
-      });
+    });
 
-      w.on('message', (result) => {
+    w.on('message', (result) => {
         console.log(result);
-      });
+    });
 
-      w.on('exit', (code) => {
+    w.on('exit', (code) => {
         if (code != 0) {
             console.log(`WARNING: thumb generator thread exited with return code: ${code}`);
         }
 
         // do not forget to ack the message once the worker has started 
         subscribe_channel.ack(msg);
-      });
+    });
 }
 
 export const listenToThumbQueue = () => {
+    console.log('Listen to thumb queue...');
     amqp.connect('amqp://localhost')
         .then(connection => {
             return connection.createChannel()
@@ -48,8 +49,8 @@ export const listenToThumbQueue = () => {
                     subscribe_channel = ch;
 
                     const opts: amqp.Options.AssertQueue = {
-                        "durable": true,
-                        "arguments": {
+                        durable: true,
+                        arguments: {
                             "x-message-deduplication": true,
                             "x-queue-type": "classic"
                         } 
@@ -62,54 +63,44 @@ export const listenToThumbQueue = () => {
                     subscribe_channel.consume(queue, buildThumbFromMessageAsync);
 
                     console.log(" [*] Waiting for thumb requests in %s. Prefetch size is: %d", queue, prefetchSize);
-                })
+                }).catch(error => console.log(error));
         })
         .catch(reason => {
             console.log('Cannot connect to rabbit thumb requests queue. ' + reason);
         });
 }
 
-const createPublishChannel = () : Promise<void> => {
-    if (typeof publish_channel === 'undefined' || publish_channel === null) {
-        return amqp.connect('amqp://localhost')
-                .then(connection => connection.createChannel())
-                .then(ch => {
-                    publish_channel = ch;                    
-                })
-    } else {
-        return new Promise<void>( (accept, ) => {
-            accept();
-        });
-    }
+const createPublishChannel = async (): Promise<amqp.Channel> => {
+        const connection = await amqp.connect('amqp://localhost')
+        return connection.createChannel();
 }
 
-export const publishToThumbQueue = (request: ThumbRequest): Promise<void> => {
-    return new Promise<void>( (resolve, reject) => {
-        createPublishChannel()
-                .then(() => {
-                    const queueOpts: amqp.Options.AssertQueue = {
-                        "durable": true,
-                        "arguments": {
-                            "x-message-deduplication": true,
-                            "x-queue-type": "classic"
-                        } 
-                    }
-                    publish_channel.assertQueue(queue, queueOpts);
+export const publishToThumbQueue = async (request: ThumbRequest): Promise<void> => {
+    const publish_channel = await createPublishChannel();
+    const queueOpts: amqp.Options.AssertQueue = {
+        durable: true,
+        arguments: {
+            "x-message-deduplication" : true
+        }
+    }
+    await publish_channel.assertQueue(queue, queueOpts);
 
-                    const opts: amqp.Options.Publish = {
-                        headers: {
-                            "x-deduplication-header": request.fullFilename
-                        }
-                    }
+    const opts: amqp.Options.Publish = {
+        headers: {
+            "x-deduplication-header": request.fullFilename,
+        },
+        persistent: true
+    }
 
-                    const msg = JSON.stringify(request);
-                    if ( !publish_channel.sendToQueue(queue, Buffer.from(msg), opts) ) {
-                        const errMsg = 'Could not send thumb request to queue !';
-                        console.error(errMsg);
-                        reject(errMsg);
-                    } else {
-                        resolve();
-                    }
-                });
+    return new Promise<void>((accept, reject) => {
+        const msg = JSON.stringify(request);
+        if (!publish_channel.sendToQueue(queue, Buffer.from(msg), opts)) {
+            const errMsg = 'Could not send thumb request to queue !';
+            console.error(errMsg);
+            reject();
+        } else {
+            publish_channel.close();
+            accept();
+        }
     });
 }
