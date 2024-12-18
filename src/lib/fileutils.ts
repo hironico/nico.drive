8aae2a334a212ebf04cb7c0f9af020c62f0be7d8
hironico <hironico.pub@outlook.com>
2024-12-18 17:57:42 +0100

    format file

--- src/lib/fileutils.ts
+++ src/lib/fileutils.ts
@@ -8,7 +8,7 @@ import { join } from 'path';
 
 // supported formats are : JPEG, PNG, WebP, AVIF, TIFF, GIF and SVG
 // see doc at : https://sharp.pixelplumbing.com/
-const supportedFormats: string[] = [ 'JPEG', 'JPG', 'PNG', 'WEBP', 'AVIF', 'TIFF', 'TIF', 'GIF', 'SVG', 'CR2', 'CR3', 'DNG'];
+const supportedFormats: string[] = ['JPEG', 'JPG', 'PNG', 'WEBP', 'AVIF', 'TIFF', 'TIF', 'GIF', 'SVG', 'CR2', 'CR3', 'DNG'];
 
 export const getFileExtention = (filename: string): string => {
     if (typeof filename === 'undefined' || filename === null) {
@@ -24,7 +24,7 @@ export const getFileExtention = (filename: string): string => {
         return null;
     }
 
-    return filename.toUpperCase().substring(index+1);
+    return filename.toUpperCase().substring(index + 1);
 }
 
 export const isRawFile = (filename: string): boolean => {
@@ -32,7 +32,7 @@ export const isRawFile = (filename: string): boolean => {
     if (extention === null) {
         return false;
     }
-    
+
     const regexp = new RegExp(/CR[0-9]/);
     return regexp.test(extention);
 }
@@ -42,7 +42,7 @@ export const isFileSupported = (filename: string): boolean => {
     if (extention === null) {
         return false;
     }
-    
+
     const formatIndex = supportedFormats.indexOf(extention);
     return formatIndex !== -1;
 }
@@ -64,15 +64,15 @@ export const md5 = (fileName: string): Promise<string> => {
             stream.pipe(hash);
 
             // quand le stream est terminé, lire le hash md5
-            stream.on('end', function() {
+            stream.on('end', function () {
                 try {
                     const md5 = hash.digest('hex');
                     resolve(md5);
-                } catch(error) { 
-                    reject(error) 
+                } catch (error) {
+                    reject(error)
                 }
             });
-        } catch(error) {
+        } catch (error) {
             console.log(`Cannot compute md5`);
             reject(error);
         }
@@ -84,39 +84,45 @@ export const md5 = (fileName: string): Promise<string> => {
  * @param dir the name of the directory to get the total size recursively
  * @returns Promise<number> containing the total of space used in bytes.
  */
-export const dirSize = (dir: string) : number => {
+export const dirSize = (dir: string): number => {
     try {
         accessSync(dir, constants.R_OK | constants.X_OK);
     } catch (error) {
-        console.log(`Cannot compute directory size for ${dir}. Access denied. Total size will be incorrect.`);
+        console.error(`Cannot compute directory size for ${dir}. Access denied. Total size will be incorrect.`);
         return 0;
     }
 
-    const files = readdirSync( dir, { withFileTypes: true } );
+    try {
+        const files = readdirSync(dir, { withFileTypes: true });
 
-    const paths = files.map(file => {
-        const path = join( dir, file.name );  
-        if ( file.isDirectory() ) {
-            return dirSize( path );
-        }
-    
-        if ( file.isFile() ) {
-            const { size } = statSync( path );        
-            return size;
-        }
-    
+        const paths = files.map(file => {
+            const path = join(dir, file.name);
+            if (file.isDirectory()) {
+                return dirSize(path);
+            }
+
+            if (file.isFile()) {
+                const { size } = statSync(path);
+                return size;
+            }
+
+            console.warn('Folder entry type is not a directory nor a file');
+            return 0;
+        });
+
+        return paths.reduce((i, size) => i + size, 0);
+    } catch (error) {
+        console.log(`Cannot read the directory: ${dir}. ${error}`);
         return 0;
-    });
-  
-    return paths.reduce( ( i, size ) => i + size, 0 );
-  }
+    }
+}
 
 /**
  * Count the number of elements in a given directory. This function is not recursive.
  * @param dir the directory to count elements from 
  * @returns number of files and folders in the directory.
  */
-export const dirElementsCount = (dir: string) : number => {
+export const dirElementsCount = (dir: string): number => {
     const entries = readdirSync(dir);
     return entries.length;
 }
